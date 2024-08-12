bc92ed54ca5da5b0577a263d0129939230ab3379
AreebaAbid0067 <aareeba006@gmail.com>
2024-08-12 13:01:32 +0300

    commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages,setup
+
+setup(
+    name = "ml_chatbot",
+    version = "0.0.0",
+    author = "Areeba Abid",
+    author_email = "aareeba006@gmail.com",
+    packages=find_packages(),
+    install_requires = []
+
+)
+
+
+
+
+
+
+
